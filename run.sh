f919ace3d2562ab88fe491bebe7545ff7c3cf57b
cxnt <pavelbartsits@gmail.com>
2022-09-29 17:02:38 +0400

    add notebook dir to entrypoint

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 cp /repo/demo.ipynb /sly-workdir/demo.ipynb
-jupyter lab --ip=0.0.0.0 --port=8000 --allow-root --ServerApp.token='' --ServerApp.password='' --ServerApp.allow_origin=* --ServerApp.base_url=$BASE_URL --ServerApp.trust_xheaders=True --ServerApp.disable_check_xsrf=True --LabApp.default_url='/lab/tree/demo.ipynb'
+jupyter lab --ip=0.0.0.0 --port=8000 --allow-root --ServerApp.token='' --ServerApp.password='' --ServerApp.notebook_dir='/sly-workdir' --ServerApp.allow_origin=* --ServerApp.base_url=$BASE_URL --ServerApp.trust_xheaders=True --ServerApp.disable_check_xsrf=True --LabApp.default_url='/lab/tree/demo.ipynb'
