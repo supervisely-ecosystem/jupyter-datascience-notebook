cb43ebf58282e3e65fc2954cf9edec79d8ed94e5
Maxim Kolomeychenko <maxim@supervise.ly>
2022-10-01 09:11:58 +0300

    dockerimage 1.0.6

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-cp /repo/demo.ipynb /sly-app-data/demo.ipynb
-jupyter lab --ip=0.0.0.0 --port=8000 --allow-root --ServerApp.token='' --ServerApp.password='' --ServerApp.notebook_dir='/sly-app-data' --ServerApp.allow_origin=* --ServerApp.base_url=$BASE_URL --ServerApp.trust_xheaders=True --ServerApp.disable_check_xsrf=True --LabApp.default_url='/lab/tree/demo.ipynb'
+python /repo/preprocessing/main.py
+jupyter lab --ip=0.0.0.0 --port=8000 --allow-root --ServerApp.token='' --ServerApp.password='' --ServerApp.notebook_dir='/sly-app-data' --ServerApp.allow_origin=* --ServerApp.base_url=$BASE_URL --ServerApp.trust_xheaders=True --ServerApp.disable_check_xsrf=True 
+#--LabApp.default_url='/lab/tree/demo.ipynb'
