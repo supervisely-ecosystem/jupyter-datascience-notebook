5406907ac8782c71e7314f953aa592615c826656
Maxim Kolomeychenko <maxim@supervise.ly>
2022-10-01 11:07:26 +0300

    update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
-printenv && \
 python ./preprocessing/main.py  && \
 jupyter lab --ip=0.0.0.0 --port=8000 --allow-root --ServerApp.token='' --ServerApp.password='' --ServerApp.notebook_dir='/sly-app-data' --ServerApp.allow_origin=* --ServerApp.base_url=$BASE_URL --ServerApp.trust_xheaders=True --ServerApp.disable_check_xsrf=True 
 #--LabApp.default_url='/lab/tree/demo.ipynb'
