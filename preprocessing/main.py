7eeb8dd25e70242d8916656026b8327ac18c041c
Maxim Kolomeychenko <maxim@supervise.ly>
2022-10-01 07:59:11 +0300

    env done

--- preprocessing/main.py
+++ preprocessing/main.py
@@ -1,4 +1,19 @@
 import os
+from dotenv import load_dotenv
 import supervisely as sly
 
-INPUT_FILE = os.environ["context.slyFile"]
+# for convenient debug, has no effect in production
+load_dotenv("preprocessing/local.env")
+load_dotenv(os.path.expanduser("~/supervisely.env"))
+
+input_file = os.environ.get("context.slyFile")
+if input_file is None:
+    sly.logger.info("App is started from ecosystem")
+    exit(0)
+
+sly.logger.info(f"App is started from context menu of file in TeamFiles: {input_file}")
+ext = sly.fs.get_file_ext(input_file)
+if ext != ".ipynb":
+    raise ValueError(
+        "JupyterLab Notebook can only be started from the context menu of '.ipynb' file or from Ecosystem"
+    )
