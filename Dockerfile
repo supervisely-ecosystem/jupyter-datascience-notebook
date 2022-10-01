63bc8745d33400b33be0aee9cdaa3c138d3701d7
Max Kolomeychenko <maxim.kolomeychenko@gmail.com>
2022-10-01 10:02:33 +0300

    Debug directory synced with Team Files (#4)
    * start processing input file
    
    * env done
    
    * wip
    
    * need tests
    
    * define entrypoint
    
    * TBD min_instance_version
    
    * _min_instance_version
    
    * dockerimage tag - 1.0.5
    
    * update dockerfile
    
    * dockerimage 1.0.6
    
    * 1.0.7
    
    * wip
    
    * update run commands
    
    * TBD min agent and instance version
    
    Co-authored-by: Maxim Kolomeychenko <maxim@supervise.ly>

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,10 @@ FROM jupyter/datascience-notebook
 
 USER root
 
-RUN pip install supervisely==6.64.0
+RUN pip install supervisely==6.64.3
 
-WORKDIR /sly-workdir
+WORKDIR /sly-app-data
 
 COPY /demo.ipynb /repo/demo.ipynb
 COPY /run.sh /repo/run.sh
+COPY /preprocessing /repo/preprocessing
---
+++ preprocessing/main.py
@@ -0,0 +1,68 @@
+import os
+
+# import subprocess
+# import shlex
+from dotenv import load_dotenv
+import supervisely as sly
+
+# for convenient debug, has no effect in production
+if sly.is_development():
+    load_dotenv("preprocessing/local.env")
+    load_dotenv(os.path.expanduser("~/supervisely.env"))
+
+input_file = os.environ.get("context.slyFile")
+team_id = int(os.environ["context.teamId"])
+
+default_nb = "demo.ipynb"
+default_path = None
+
+if input_file is None:
+    sly.logger.info("App is started from ecosystem")
+    default_path = os.path.join(sly.app.get_synced_data_dir(), default_nb)
+    sly.fs.copy_file(src=default_nb, dst=default_path)
+else:
+    sly.logger.info(f"App is started from context menu of file in TeamFiles: {input_file}")
+    ext = sly.fs.get_file_ext(input_file)
+    if ext != ".ipynb":
+        sly.logger.warn(
+            "JupyterLab Notebook can only be started from the context menu of '.ipynb' file or from Ecosystem. \n"
+            + f"Default demo notebook will be started: {default_path}"
+        )
+    else:
+        default_nb = sly.fs.get_file_name_with_ext(input_file)
+        default_path = os.path.join(sly.app.get_synced_data_dir(), default_nb)
+
+        api = sly.Api()
+        api.file.download(
+            team_id,
+            remote_path=input_file,
+            local_save_path=os.path.join(sly.app.get_synced_data_dir(), default_nb),
+        )
+
+sly.logger.info(f"Default notebook: {default_path}")
+
+# try:
+#     process = subprocess.run(
+#         shlex.split(
+#             f'jupyter lab \
+#                 --ip=0.0.0.0 \
+#                 --port=8000 \
+#                 --allow-root \
+#                 --ServerApp.token="" \
+#                 --ServerApp.password="" \
+#                 --ServerApp.notebook_dir="{sly.app.get_data_dir()}" \
+#                 --ServerApp.allow_origin=* \
+#                 --ServerApp.base_url=$BASE_URL \
+#                 --ServerApp.trust_xheaders=True \
+#                 --ServerApp.disable_check_xsrf=True \
+#                 --LabApp.default_url="/lab/tree/{default_nb}"'
+#         ),
+#         stdout=subprocess.PIPE,
+#         stderr=subprocess.PIPE,
+#         universal_newlines=True,
+#     )
+#     process.check_returncode()
+#     sly.logger.info(f"jupyter lab have been successfully finished")
+# except Exception as e:
+#     print(repr(e))
+#     raise e
